559862f2bc3e6fbe561e4b2e2a21e27b744a4efe
isaac-oliveira <isaacsantos9876@gmail.com>
2020-04-04 22:40:26 -0300

    Rotas autenticadas

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,13 +5,15 @@ import SessionController from './controllers/SessionController';
 import OperationController from './controllers/OperationController';
 import TransferController from './controllers/TransferController';
 
+import authenticate from './middleware/authenticate';
+
 const routes = express.Router();
 
 routes.post('/register', UserController.store);
 routes.get('/users', UserController.index);
 routes.post('/login', SessionController.store);
-routes.put('/operation/:id', OperationController.update);
-routes.get('/operations', OperationController.index);
-routes.put('/transfer/:id', TransferController.update);
+routes.put('/operation/:id', authenticate, OperationController.update);
+routes.get('/operations', authenticate, OperationController.index);
+routes.put('/transfer/:id', authenticate, TransferController.update);
 
 export default routes;
